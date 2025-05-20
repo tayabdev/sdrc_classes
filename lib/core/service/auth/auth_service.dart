b64afc06505cab3bd933add5ec6e96422a09246e
tayab009 <tayabfarooq009@gmail.com>
2025-05-20 14:26:55 +0500

    grab user id and will working on linking users to firestore

--- lib/core/service/auth/auth_service.dart
+++ lib/core/service/auth/auth_service.dart
@@ -1,13 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
   // we will authenticate user--login/signup...
   // Create User
   FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
 
   Future<void> createUser(String email, String password) async {
-    await firebaseAuth.createUserWithEmailAndPassword(
-        email: email, password: password);
+    UserCredential userCredential = await firebaseAuth
+        .createUserWithEmailAndPassword(email: email, password: password);
+
+    if (userCredential.user != null) {
+      print(userCredential.user!.uid);
+      print('=======================');
+      print(userCredential.user);
+    }
+
     // try {
 
     // } catch (e) {
